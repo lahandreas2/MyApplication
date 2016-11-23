db557e7cafe0135a4dd271fcb11fb8aac0e0ccf3
lahandreas1 <lah.andreas1@gmail.com>
2016-11-23 12:45:50 +0100

    df

---
+++ app/src/main/java/com/example/danica/myapplication/sadf.java
@@ -0,0 +1,13 @@
+package com.example.danica.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sadf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sadf);
+    }
+}
